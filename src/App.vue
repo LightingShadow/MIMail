b809dbd42b868fe558da0b33d4ea47131824c284
IGine_ <453392153@qq.com>
2020-05-10 12:52:32 +0800

    静态资源和基本结构

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id='app'>
+    <router-view>
+      
+    </router-view>
+  </div>
+</template>
+
+<script>
+import jsonp from 'jsonp';
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+      let url = '/static/sea_config.js?v=202005091600';
+      jsonp(url,(err,res)=> {
+        let {alias} = res;
+        console.log(alias);
+      });
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
